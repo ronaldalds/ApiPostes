a5c30995d2d10677a1734048ce8a2b48e90a09a9
ronaldalds <43586601+ronaldalds@users.noreply.github.com>
2022-09-29 08:37:29 -0300

    primeiro commit para FastAPI

--- apipole/api/serializers.py
+++
@@ -1,8 +0,0 @@
-from apipole import models
-from rest_framework import serializers
-
-
-class PoleSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = models.Pole
-        fields = '__all__'
--- apipole/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ApipoleConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apipole'
